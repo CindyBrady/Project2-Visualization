c788dd788dc37d774b949a40686befbbf366b55c
CindyBrady <cindybrady777@gmail.com>
2021-01-25 20:58:01 -0500

    Update app.py
    Reverting back app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return ('index.html')
+@app.route("/") 
+def home_view(): 
+        return "<h1>Welcome to The Great Outdoors!</h1>"
