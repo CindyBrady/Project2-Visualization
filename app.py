43d4a960aa0664efbe0631b5f52120d075e59583
CindyBrady <cindybrady777@gmail.com>
2021-01-26 19:43:55 -0500

    Update app.py
    Updated app.py

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/')
+def dashboard():
+    
+    return render_template("dashboard.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
