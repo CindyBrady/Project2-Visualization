8a5f85c6cbe690da2c867caa65702a1ffcf3deb7
Conner Mixon <PuckishKnave@gmail.com>
2021-01-26 20:22:11 -0500

    Update app.py
    testing for heroku deployment

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route("/templates/dashboard")
+@app.route('/dashboard/')
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
