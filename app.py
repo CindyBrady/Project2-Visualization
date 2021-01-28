967575d061d7786c745f6f61dc4f57dd5b851aa2
CindyBrady <cindybrady777@gmail.com>
2021-01-27 20:03:29 -0500

    Update app.py
    Updated app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-# from flask import Flask, render_template
-# app = Flask(__name__)
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-# @app.route('/dashboard/')
-# def dashboard():
-#     return render_template('dashboard.html')
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
 
-# @app.route('/map/')
-# def map():
-#     return render_template('map.html')
+@app.route('/map/')
+def map():
+    return render_template('map.html')
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
   
